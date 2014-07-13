722afb3299618b2a240992eebbc29cb0fe36ff23
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-07-13 18:09:07 -0400

    #12: define Immediately-Invoked Function wrapper

--- src/js/getusermedia.js
+++ src/js/getusermedia.js
@@ -2,16 +2,8 @@
  * detect_getusermedia.js: add respective classnames for 'getUserMedia', or 'Flash' support
  */
 
-$( document ).ready(function() {
+// Immediately Invoked Functions
 
-// getUserMedia() feature detection
-  navigator.getUserMedia_ = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia );
+(function (window, document) {
 
-  if ( navigator.getUserMedia_ ) {
-    $('microphone').addClass('getusermedia_supported');
-  }
-  else {
-    $('microphone').addclass('flash_supported');
-  }
-
-});
+})(this, document);  // parameters passed into 'Immediately Invoked Functions' (i.e. window = this)
