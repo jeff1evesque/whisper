b9a3653ae782c00172d4223c539af5de80bae277
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-07-14 12:48:10 -0400

    #33: Fix javascript reference in test.php

--- tests/php/test.php
+++ tests/php/test.php
@@ -8,9 +8,9 @@
 
 <html>
   <head>
-    <script type='text/javascript' src='src/js/html_microphone.js'></script>
-    <script type='text/javascript' src='src/js/initializer.js'></script>
-    <script type='text/javascript' src='src/js/audioRecorder.js'></script>
+    <script type='text/javascript' src='../../src/js/html_microphone.js'></script>
+    <script type='text/javascript' src='../../src/js/initializer.js'></script>
+    <script type='text/javascript' src='../../src/js/audioRecorder.js'></script>
   </head>
 
   <body>
