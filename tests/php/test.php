5c0a4db6340193990d612e02f56f95ba77914e03
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-07-14 05:31:00 -0400

    #24: Add audioRecorder.js reference

--- tests/php/test.php
+++ tests/php/test.php
@@ -10,6 +10,7 @@
   <head>
     <script type='text/javascript' src='src/js/html_microphone.js'></script>
     <script type='text/javascript' src='src/js/initializer.js'></script>
+    <script type='text/javascript' src='src/js/audioRecorder.js'></script>
   </head>
 
   <body>
