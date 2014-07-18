339108c9fae30342c20443c474e4fe57d2714b13
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-07-18 09:08:51 -0400

    #65: define port 90001 for websocket protocol

--- websocket/server.py
+++ websocket/server.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
 
    log.startLogging(sys.stdout)
 
-   factory = WebSocketServerFactory("ws://localhost:9000", debug = False)
+   factory = WebSocketServerFactory("ws://localhost:90001", debug = False)
    factory.protocol = MyServerProtocol
 
    reactor.listenTCP(9000, factory)
