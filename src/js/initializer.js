1859725e6364cd2c8b38b0d09b7910a9e61ea9b4
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-07-18 09:30:38 -0400

    #67: Proper javascript comment

--- src/js/initializer.js
+++ src/js/initializer.js
@@ -3,10 +3,13 @@
  */
 
 var websocket = null;
+
 // URL of the WebSocket server (ipsikit)
-#var websocket_uri = "ws://108.59.3.115:18875";
+//var websocket_uri = "ws://108.59.3.115:18875";
+
 // URL of the WebSocket server (whisper)
 var websocket_uri = "ws://localhost:90001";
+
 var audio_context;
 var recorder = null;
 // You might need to change this to full path
