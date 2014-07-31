c3aecddfccf7d8cf48e557e0437798f47c1856f4
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-07-31 09:31:17 -0400

    #97: add simplified 'initializer.js'

---
+++ src/js/initializer.js
@@ -0,0 +1,25 @@
+/**
+ * initializer.js:
+ */
+
+function send() {
+  msg = "TEST: 'initializer'";
+  sock.send(msg);
+};
+
+var sock = null;
+sock = new WebSocket("ws://localhost:9000");
+console.log("Websocket created...");
+
+sock.onopen = function() {
+  console.log("connected to server");
+  sock.send("CONNECTED TO YOU");
+}
+
+sock.onclose = function(e) {
+  console.log("connection closed (" + e.code + ")");
+}
+
+sock.onmessage = function(e) {
+  console.log("message received: " + e.data);
+}
