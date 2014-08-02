200a6e5fc8ace2cbacb2fcdb7a917d743f5564b6
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-08-02 10:34:59 -0400

    #99: initializer.js, add postMessage() and respective comments

--- src/js/webworker_stream_audio.js
+++ src/js/webworker_stream_audio.js
@@ -2,10 +2,17 @@
  * webworker_audio_stream.js:
  */
 
+$(document).ready(function() {
 
-// Function Definitions:
+// Stream Data: Each time 'postMessage()' executes in this script, the associated
+//              '.onmessage()' declared within the script that instantiates this
+//              WebWorker script will execute, respectively.
+  postMessage(setTimeout('randomByte()', 1000));
 
+// Function Definitions
   function randomByte() {
     return Math.floor(Math.random()*256).toString(2);
   }
+
+});
 
