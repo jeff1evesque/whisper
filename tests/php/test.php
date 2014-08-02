cbf03977158e73eb799ed866558a90a123ce3b5a
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-08-02 17:15:17 -0400

    #100: test.php, order 'audioRecorder.js' before 'initializer.js'

--- tests/php/test.php
+++ tests/php/test.php
@@ -10,8 +10,8 @@
   <head>
     <script src='../../src/js/jquery-1.8.3.js'></script>
     <script src='../../src/js/html_microphone.js'></script>
-    <script src='../../src/js/initializer.js'></script>
     <script src='../../src/js/audioRecorder.js'></script>
+    <script src='../../src/js/initializer.js'></script>
   </head>
 
   <body>
