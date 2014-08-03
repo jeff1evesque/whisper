a0feb250c46760a32c0e01d8f3926347182193e9
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-08-03 18:25:00 -0400

    #100: server.py, list websocket interfaces in comment

--- websocket/server.py
+++ websocket/server.py
@@ -1,7 +1,5 @@
-"""
-@server.py
-This file contains the server side of the Autobahn websocket service
-"""
+## @server.py
+#  This file contains the server side of the Autobahn websocket service
 
 import json
 from twisted.internet import reactor
@@ -22,12 +20,35 @@ from autobahn.twisted.websocket import WebSocketServerFactory, \
 #
 #    # Prints entire JSON-formatted string nicely
 #    print json.dumps(decoded, sort_keys=True, indent=4)
+#
+#  onConnect(self, requestOrResponse) callback fired during WebSocket opening handshake
+#    when client connects, or when server connection established (by a client with 
+#    response from server).
+#
+#  onOpen(self) callback fired when initial WebSocket opening handshake was completed.
+#    You now can send and receive WebSocket messages.
+#
+#  sendMessage(self, payload, isBinary = False, fragmentSize = None, sync = False,
+#    doNotCompress = False) send a WebSocket message over the connection to the peer.
+#
+#  onMessage(self, payload, isBinary) callback fired when a complete WebSocket message
+#    was received.
+#
+#  sendClose(self, code = None, reason = None) starts WebSocket closing handshake.
+#
+#  onClose(self, wasClean, code, reason) callback when the WebSocket connection has been
+#    closed (WebSocket closing handshake has been finished or the connection was closed
+#    uncleanly).
+#
+#  More interfaces are available:
+#
+#  github.com/tavendo/AutobahnPython/blob/master/autobahn/autobahn/websocket/interfaces.py
 
 class StreamingServerProtocol(WebSocketServerProtocol):
 
    def onMessage(self, msg, binary):
-      WebSocketServerProtocol.onMessageBegin(self, binary)
       print 'sending echo:', msg
+      # send 'msg' back to the client (i.e. javascript)
       self.sendMessage(msg, binary)
 
 if __name__ == '__main__':
