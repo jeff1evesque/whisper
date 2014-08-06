803b84cca89357c911b2e051d78e0a9ed2f00edd
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-08-06 14:23:21 -0400

    #118: add additional script comment, and fix indentation

--- src/js/audioRecorder.js
+++ src/js/audioRecorder.js
@@ -16,80 +16,97 @@
  *                    'WebWorker' Constructor by implementing the onmessage message:
  *  
  *                    onmessage = function(event) {console.log(event.data)}
+ *
+ *  @AudioRecorder(source, websocket, cfg, clb)
+ *
+ *      @source a 'MediaStreamAudioSourceNode' object
+ *      @websocket a defined websocket object
+ *      @cfg audio recorder configurations
+ *      @clb audio recorder callback
  */
 
 (function(window){
-    var AudioRecorder = function(source, websocket, cfg, clb) {
-	this.websocket = websocket;
-	var callback = clb || function (x) { };
-	var config = cfg || {};
-	var inputBufferLength = config.bufferLen || 4096;
-	var outputBufferLength = config.outputBufferLength || 4000;
-	var recording = false;
-	var volumeMax = -1;
-	var receivedData = false;
-	this.context = source.context;
-	this.node = this.context.createScriptProcessor(inputBufferLength, 2, 2);
-	var worker = new Worker(config.worker || WEBWORKER_AUDIO_RECORDER);
-	worker.postMessage({
-	    command: 'init',
-	    config: {
-		sampleRate: this.context.sampleRate,
-		outputBufferLength: outputBufferLength
-	    }
-	});
-	this.node.onaudioprocess = function(e){
-	    if (recording) {
-		worker.postMessage({
-		    command: 'record',
-		    buffer: [
-			e.inputBuffer.getChannelData(0),
-			e.inputBuffer.getChannelData(1)
-		    ]
-		});
-	    }
-	};
+  var AudioRecorder = function(source, websocket, cfg, clb) {
+    this.websocket = websocket;
 
-	this.start = function(){
-	    this.websocket.send("start");
-	    volumeMax = 0;
-	    recording = true;
-	    receivedData = false;
-	    callback({volumeMax: -1});
-	};
-	
-	this.stop = function(){
-	    recording = false;
-	    this.websocket.send("stop");
-	    if (receivedData) {
-		callback({volumeMax: volumeMax});
-	    }
-	    worker.postMessage({ command: 'getWave' });
-	    worker.postMessage({ command: 'clear' });
-	};
+  // Assign 'noop' function / empty object if cfg, or clb is undefined
+    var callback = clb || function () { };
+    var config = cfg || {};
 
-	this.cancel = function(){
-	    recording = false;
-	    this.websocket.send("cancel");
-	    worker.postMessage({ command: 'clear' });
-	};
+    var inputBufferLength = config.bufferLen || 4096;
+    var outputBufferLength = config.outputBufferLength || 4000;
+    var recording = false;
+    var volumeMax = -1;
+    var receivedData = false;
+    this.context = source.context;
+    this.node = this.context.createScriptProcessor(inputBufferLength, 2, 2);
+    var worker = new Worker(config.worker || WEBWORKER_AUDIO_RECORDER);
+    worker.postMessage({
+      command: 'init',
+      config: {
+        sampleRate: this.context.sampleRate,
+        outputBufferLength: outputBufferLength
+      }
+    });
 
-	myrecorder = this;
-	worker.onmessage = function(e){
-	    if ((e.data.command == 'newVolume') && recording) {
-		volumeMax = Math.max(volumeMax, e.data.data);
-		callback({volume: e.data.data});
-	    } else if ((e.data.command == 'newBuffer') && recording) {
-		receivedData = true;
-		myrecorder.websocket.send(e.data.data);
-	    } else if (e.data.command == 'newWave') {
-		var url = URL.createObjectURL(e.data.blob);
-		callback({audio: url});
-	    }
-	};
-	source.connect(this.node);
-	this.node.connect(this.context.destination);
+    this.node.onaudioprocess = function(e) {
+      if (recording) {
+        worker.postMessage({
+          command: 'record',
+          buffer: [
+            e.inputBuffer.getChannelData(0),
+            e.inputBuffer.getChannelData(1)
+          ]
+        });
+      }
     };
-    window.AudioRecorder = AudioRecorder;
+
+    this.start = function() {
+      this.websocket.send("start");
+      volumeMax = 0;
+      recording = true;
+      receivedData = false;
+      callback({volumeMax: -1});
+    };
+	
+    this.stop = function() {
+      recording = false;
+      this.websocket.send("stop");
+        if (receivedData) {
+          callback({volumeMax: volumeMax});
+        }
+        worker.postMessage({ command: 'getWave' });
+        worker.postMessage({ command: 'clear' });
+      };
+
+      this.cancel = function() {
+      recording = false;
+      this.websocket.send("cancel");
+      worker.postMessage({ command: 'clear' });
+    };
+
+    myrecorder = this;
+    worker.onmessage = function(e) {
+      if ((e.data.command == 'newVolume') && recording) {
+        volumeMax = Math.max(volumeMax, e.data.data);
+        callback({volume: e.data.data});
+      }
+      else if ((e.data.command == 'newBuffer') && recording) {
+        receivedData = true;
+        myrecorder.websocket.send(e.data.data);
+      }
+      else if (e.data.command == 'newWave') {
+        var url = URL.createObjectURL(e.data.blob);
+        callback({audio: url});
+      }
+    };
+
+    source.connect(this.node);
+    this.node.connect(this.context.destination);
+
+  };
+
+  window.AudioRecorder = AudioRecorder;
+
 })(window);
 
