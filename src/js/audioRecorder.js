598b86f8d6b577d5a6eb1153cd0686fb3f177d69
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-08-07 11:26:05 -0400

    #118: reformat comment

--- src/js/audioRecorder.js
+++ src/js/audioRecorder.js
@@ -1,60 +1,56 @@
 /**
- *  audioRecorder.js: defines various 'stream' configurations, and sends the data to
- *                    the corresponding WebWorker defined in 'audioRecorderWorker.js'.
+ *  audioRecorder.js: defines various 'stream' configurations, and sends the data to the
+ *                    corresponding WebWorker defined in 'audioRecorderWorker.js'.
  *
- *                    A WebWorker is an HTML5 feature that allows javascript to be
- *                    run in the background independent of other scripts (threads),
- *                    without affecting the performance of the page.  This portion
- *                    is considered the 'WebWorker Constructor'.
+ *                    A WebWorker is an HTML5 feature that allows javascript to be run in the
+ *                    background independent of other scripts (threads), without affecting the
+ *                    performance of the page.  This portion is considered the 'WebWorker
+ *                    Constructor'.
  *
- *  @worker.onmessage is an event listener to the 'WebWorker'.  The 'event.data'
+ *  @worker.onmessage is an event listener to the 'WebWorker'.  The 'event.data', and
  *                    'event.data' parameter is used to acquire data from the WebWorker.
  *
- *  @worker.postMessage('YOUR-DATA-HERE') sends data to the WebWorker.
- *                    However, if no parameter is supplied, postMessage() simply starts
- *                    the WebWorker.  The WebWorker receives data from this
- *                    'WebWorker' Constructor by implementing the onmessage message:
+ *  @worker.postMessage('YOUR-DATA-HERE') sends data to the WebWorker.  However, if no
+ *                    parameter is supplied, postMessage() simply starts the WebWorker.
+ *                    The WebWorker receives data from this 'WebWorker Constructor' by
+ *                    implementing the onmessage message:
  *  
  *                    onmessage = function(event) {console.log(event.data)}
  *
- *  @AudioRecorder(source, websocket, cfg, clb)
+ *  @source a 'MediaStreamAudioSourceNode' object passed in from the instatiation of an
+ *    AudioRecorder within 'initializer.js'.  The 'MediaStreamAudioSourceNode' object is
+ *    AudioNode that acts as an 'audio source', it inherits the 'AudioNode.context' property.
  *
- *      @source a 'MediaStreamAudioSourceNode' object passed in from the instatiation of
- *        AudioRecorder within 'initializer.js'.  The 'MediaStreamAudioSourceNode' object is
- *        an AudioNode that acts as an 'audio source', it inherits the 'AudioNode.context'
- *        property.
+ *  @websocket a defined websocket object
+ *  @cfg audio recorder configurations
+ *  @clb audio recorder callback
  *
- *      @websocket a defined websocket object
- *      @cfg audio recorder configurations
- *      @clb audio recorder callback
+ *  @this.context.createScriptProcessor(bufferSize, inputChannels, outputChannels) is a method
+ *    of the 'AudioContext' interface, and creates a 'ScriptProcessorNode' used for direct
+ *    audio processing.
  *
- *      @this.context.createScriptProcessor(bufferSize, inputChannels, outputChannels) is a
- *        method of the 'AudioContext' interface, and creates a 'ScriptProcessorNode' used for
- *        direct audio processing.
+ *    @bufferSize buffer size in units of sample-frames.  If specified, the bufferSize must be
+ *      one of the following: 256, 512, 102, 2048, 4096, 8192, 16384.  If it's not passed in,
+ *      or if the value is 0, then the implementation will choose the best buffer size for the
+ *      given environment (recommended).  This value controls how frequently the onaudioprocess
+ *      event handler is called and how many sample-frames need to be processed for each call.
+ *      Lower values for bufferSize will result in a lower (better) latency. Higher values will
+ *      be necessary to avoid audio breakup and glitches.
  *
- *          @bufferSize buffer size in units of sample-frames.  If specified, the bufferSize
- *            must be one of the following: 256, 512, 102, 2048, 4096, 8192, 16384.  If it's
- *            not passed in, or if the value is 0, then the implementation will choose the
- *            best buffer size for the given environment (recommended).  This value controls
- *            how frequently the onaudioprocess event handler is called and how many
- *            sample-frames need to be processed for each call.  Lower values for bufferSize
- *            will result in a lower (better) latency. Higher values will be necessary to
- *            avoid audio breakup and glitches.
+ *      A 'sample' is a single float32 value that represents the value of the audio stream at
+ *      each specific point in time, in a specific channel.A 'sample-frame' is the set of all
+ *      values for all channels that will play at a each specific point in time: all the
+ *      'samples' of all the channels that play at the same time (one for mono, two for a
+ *      stereo sound, etc.).
  *
- *            A 'sample' is a single float32 value that represents the value of the audio
- *            stream at each specific point in time, in a specific channel.A 'sample-frame' is
- *            the set of all values for all channels that will play at a each specific point
- *            in time: all the 'samples' of all the channels that play at the same time (one
- *            for mono, two for a stereo sound, etc.).
+ *      A 'sample rate' is the number of 'sample frames' per second, used by all nodes in this
+ *      audio context.
  *
- *            A 'sample rate' is the number of 'sample frames' per second, used by all nodes
- *            in this audio context.
+ *    @inputChannels (optional) defaults to 2, and determines the number of channels for this
+ *      node's input.
  *
- *          @inputChannels (optional) defaults to 2, and determines the number of channels
- *            for this node's input.
- *
- *          @outputChannels (optional) defaults to 2, and determines the number of channels
- *            for this nodes output.
+ *    @outputChannels (optional) defaults to 2, and determines the number of channels for this
+ *      nodes output.
  */
 
 (function(window){
