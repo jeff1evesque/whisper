b8c1cb730ddec21fc4b7072df68791b6d98a41dc
Jeffrey Levesque <jeff1evesque@yahoo.com>
2014-08-08 05:54:28 -0400

    #119: minor language, and spacing changes

--- src/js/audioRecorderWorker.js
+++ src/js/audioRecorderWorker.js
@@ -1,22 +1,19 @@
 /**
- *  audioRecorderWorker.js: is a 'WebWorker' for the 'WebWorker Constructor' the declared
- *                          in 'audioRecorder.js'.
+ *  audioRecorderWorker.js: is a 'WebWorker' for the 'WebWorker Constructor' the declared in
+ *                          'audioRecorder.js'.
  *
- *                          A WebWorker is an HTML5 feature that allows javascript to be
- *                          run in the background independent of other scripts (threads),
- *                          without affecting the performance of the page.  This portion
- *                          is considered the 'Worker'.
+ *                          A WebWorker is an HTML5 feature that allows javascript to be run in the
+ *                          background independent of other scripts (threads), without affecting
+ *                          the performance of the page.  This portion is considered the 'Worker'.
  *
- *  @onmessage is an event listener to the 'WebWorker Constructor'.  The 'event.data'
- *                          parameter is used to acquire data from the 'WebWorker
- *                          Constructor'.
+ *  @onmessage is an event listener to the 'WebWorker Constructor'.  The 'event.data' parameter is
+ *                          used to acquire data from the 'WebWorker Constructor'.
  *
- *  @postMessage('YOUR-DATA-HERE') sends data to the 'WebWorker Constructor', and must be
- *                          defined in the 'onmessage' function. The 'WebWorker 
- *                          Constructor' can receive data from this script by implementing
- *                          the following:
+ *  @postMessage('YOUR-DATA-HERE') sends data to the 'WebWorker Constructor', and must be defined
+ *                          in the 'onmessage' function. The 'WebWorker  Constructor' can receive
+ *                          data from this script by implementing the following:
  *
- *    [constructor].onmessage = function(event) { console.log(event.data) }
+ *                          [worker].onmessage = function(event) { console.log(event.data) }
  */
 
     var recordingBuffer = [],
